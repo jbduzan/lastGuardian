f24826fc48d7f3c770220554c39a10b404a507ae
jbduzanw <92720743+jbduzanw@users.noreply.github.com>
2024-07-21 20:55:45 +0200

    feat: scale weapon when firing

--- player/weaponAnchor.gd
+++ player/weaponAnchor.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 @onready var spawner: SpawnerComponent = $SpawnerComponent
+@onready var scaler: ScaleComponent = $ScaleComponent
 
 func _process(delta):
 	rotation = global_position.angle_to_point(get_global_mouse_position())
 
 func shoot():
 	spawner.spawnWithRotation(rotation, $weaponMuzzle.global_position)
+	scaler.tween_scale()
 	
